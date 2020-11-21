1341a5d333e486b2bf5ff8f4667311723e6f09cb
ETR <edcarroll34@gmail.com>
2020-11-21 14:15:04 -0500

    re-set project, added intial navigation elements

---
+++ app.js
@@ -0,0 +1,20 @@
+const appDiv = document.querySelector('.app');
+const appHeader = document.querySelector('.header');
+
+(() => {
+    navElement();
+})();
+
+function nav(){
+    return `
+    <div class="nav__section">
+        <h4 class="nav__section__comic">Pick a new comic!</h4>
+        <h4 class="nav__section__api">Marvel API</h4>
+    </div>
+    `
+}
+
+function navElement(){
+    appHeader.innerHTML= nav();
+}
+    
