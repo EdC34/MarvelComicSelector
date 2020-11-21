92fa83040e1c3dc55505fb9014bac0e3aaea5b96
ETR <edcarroll34@gmail.com>
2020-11-21 18:53:10 -0500

    basic structure and alert in place

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const navElement = function(){
     appHeader.innerHTML= nav();
     comicBtn = document.querySelector('.nav__section__comic');
     comicBtn.addEventListener('click', function(){
-        alert('here is a new comic')
+        alert('new comic here')
+        getComic()
+        loadComic()
     })
 }
 
@@ -14,32 +16,36 @@ const nav = function(){
     return `
     <div class="nav__section">
     
-    <h4 class="nav__section__items nav__section__comic">Pick a new comic!</h4>
-    <h4 class="nav__section__items nav__section__api">Marvel API</h4>
+    <h3 class="nav__section__items nav__section__comic">Pick a new comic!</h3>
+    <a class="nav__section_api-link" href="https://developer.marvel.com/documentation/getting_started" target="_blank"></a><h3 class="nav__section__items nav__section__api">Marvel API</h3>
     </div>
     `
 }
 
 
 const getComic = function(){
-    fetch('http://gateway.marvel.com/v1/public/comics?ts=1&apikey=020fa7dbd3dcaff71a1a678e2899dc21&hash=0252bc04dbc340a4b45643a15f89063f title:*&sort=random&size=1')
+    fetch(`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=020fa7dbd3dcaff71a1a678e2899dc21&hash=4b613b8ecf29c7f2cc38c226e6fa0aea title:*&sort=random&size=1`)
     .then(res => res.json())
-    .then(comic => console.log(comic))
+    .then(comic => {
+        const comicTitle = comic.records[0].title;
+        const comicIssue = comic.records[0].issueNumber;
+        const comicImageURL = comic.records[0].thumbnail;
+        const comicURL = comic.records[0].url;
+        console.log(comicTitle);
+        console.log(comicIssue);
+        console.log(comicImageURL);
+        console.log(comicURL);
+        appDiv.innerHTML = loadComic(comicTitle, comicIssue, comicImageURL, comicURL);
+    })
     .catch(err => console.log(err))
 }
 
-navElement();
+const loadComic = function(comicTitle, comicIssue, comicImageURL, comicURL){
+            return `
+            <h3 class="app__comic-title">${comicTitle}</h3>
+            <h3 class="app__comic-issue">${comicIssue}</h3>
+            <a class="app__comic-url" href='${comicURL}' target='_blank'><img class="app__comic-image-url" src='${comicImageURL}'></a>
+            `
+        }
 
-// function nav(){
-//     return `
-//     <div class="nav__section">
-//         <h4 class="nav__section__items nav__section__comic">Pick a new comic!</h4>
-//         <h4 class="nav__section__items nav__section__api">Marvel API</h4>
-//     </div>
-//     `
-// }
-
-// function navElement(){
-//     appHeader.innerHTML= nav();
-// }
-    
+navElement();  
