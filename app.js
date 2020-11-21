033a1c3b66c08227ce1d7d0369664c5cdf37ad24
ETR <edcarroll34@gmail.com>
2020-11-21 14:28:01 -0500

    nav bar changes colours when you hover over it

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const appHeader = document.querySelector('.header');
 function nav(){
     return `
     <div class="nav__section">
-        <h4 class="nav__section__comic">Pick a new comic!</h4>
-        <h4 class="nav__section__api">Marvel API</h4>
+        <h4 class="nav__section__items nav__section__comic">Pick a new comic!</h4>
+        <h4 class="nav__section__items nav__section__api">Marvel API</h4>
     </div>
     `
 }
