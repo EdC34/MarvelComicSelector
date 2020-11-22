d5d598a6edd33a30d2655e0c5ab4317787df2144
ETR <edcarroll34@gmail.com>
2020-11-22 12:44:47 -0500

    styling

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const appDiv = document.querySelector('.app');
 const appHeader = document.querySelector('.header');
+const publicKey = "020fa7dbd3dcaff71a1a678e2899dc21";
+const hash = "383389ef7c69305f8ac2cea4123e36d5";
 let comicBtn;
 
 const navElement = function(){
@@ -17,20 +19,21 @@ const nav = function(){
     <div class="nav__section">
     
     <h3 class="nav__section__items nav__section__comic">Pick a new comic!</h3>
-    <a class="nav__section_api-link" href="https://developer.marvel.com/documentation/getting_started" target="_blank"></a><h3 class="nav__section__items nav__section__api">Marvel API</h3>
+    <a class="nav__section_api-link" href=" " target="_blank"></a><h3 class="nav__section__items nav__section__api-link"> Marvel API</h3>
     </div>
     `
 }
 
+ 
 
 const getComic = function(){
-    fetch(`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=020fa7dbd3dcaff71a1a678e2899dc21&hash=4b613b8ecf29c7f2cc38c226e6fa0aea title:*&sort=random&size=1`)
+    fetch(`http(s)://gateway.marvel.com/v1/public/comics?ts=1&apikey=${publicKey}&hash=${hash}`)
     .then(res => res.json())
     .then(comic => {
-        const comicTitle = comic.records[0].title;
-        const comicIssue = comic.records[0].issueNumber;
-        const comicImageURL = comic.records[0].thumbnail;
-        const comicURL = comic.records[0].url;
+        const comicTitle = comic.title;
+        const comicIssue = comic.issueNumber;
+        const comicImageURL = comic.thumbnail;
+        const comicURL = comic.url;
         console.log(comicTitle);
         console.log(comicIssue);
         console.log(comicImageURL);
